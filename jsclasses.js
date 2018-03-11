95194430d953499364ed51937813a8bd41e742c9
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-03-11 13:40:08 -0400

    Update jsclasses.js

--- jsclasses.js
+++ jsclasses.js
@@ -1,1 +1,38 @@
+function setClasses(el,c) {
+el.setAttribute("class",c.join(" "));
+}
 
+function getClassOf(element) {
+  var classesofelement;
+  for (var i=0; i<element.attributes.length; i++) {
+    if (element.attributes[i].name === "class") {
+      classesofelement = element.attributes[i].value;
+    }
+  }
+  return classesofelement.split(" ");
+}
+var classlist = [];
+
+function newClass(name, info) {
+  classlist.push(name);
+  classlist.push(JSON.parse("info"));
+}
+// use the following variable set to set the node of the style tag to use for CSS edits.
+var stylenode = 0
+function applyClass(el,c) {
+  document.getElementsByTagName("style")[stylenode].innerHTML += el.tagName.toLowerCase() + " {" + c.innerCSS + "}";
+  el.setAttribute("class",c.classnames.join(" "));
+}
+
+function applyClasses() {
+  var elementlist = document.querySelectorAll("*");
+  var elementclasslist;
+  for (var z=0; z<elementlist.length; z++) {
+    elementclasslist = getClassOf(elementlist[z]);
+    for (var l=0; l<elementclasslist.length; l++) {
+      if (classlist.indexOf(elementclasslist[l])!==-1) {
+        applyClass(elementlist[z],classlist[classlist.indexOf(elementclasslist[l])+1]);
+      }
+    }
+  }
+}
