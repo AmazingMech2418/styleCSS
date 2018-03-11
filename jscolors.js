2dff7111e90d59d92eca53c0193cb906598727c0
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-03-11 13:47:25 -0400

    Update jscolors.js

--- jscolors.js
+++ jscolors.js
@@ -1,1 +1,4 @@
-
+function newColor(name, csscode) {
+  newClass(name+"-text", '{"innerCSS":"color:'+csscode+';","classnames":[]}');
+  newClass(name+"-back", '{"innerCSS":"background-color:'+csscode+';","classnames":[]}');
+}
